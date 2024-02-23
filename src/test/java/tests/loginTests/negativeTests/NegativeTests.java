b8e3f8f1131c55bfb80083e03cb406287d62b66c
Netanel Harush <netanelharush2@gmail.com>
2024-02-23 14:01:57 +0200

    Refactor + Add tests + Update dependencies

--- src/test/java/tests/loginTests/negativeTests/NegativeTests.java
+++ src/test/java/tests/loginTests/negativeTests/NegativeTests.java
@@ -4,28 +4,61 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NegativeTests {
+    private static final String BASE_URL = "https://www.saucedemo.com/";
+    private static final String LOCKED_OUT_ERROR_MESSAGE = "Epic sadface: Sorry, this user has been locked out.";
+    private static final String NO_MATCH_CREDENTIALS_ERROR_MESSAGE = "Epic sadface: Username and password do not match any user in this service";
+    private static final String EMPTY_PASSWORD_ERROR_MESSAGE = "Epic sadface: Password is required";
+    private static final String EMPTY_USERNAME_ERROR_MESSAGE = "Epic sadface: Username is required";
+
 
     @Test(testName = "Validate scenario with locked_out_user")
-    public void loginWithLockerUser() {
+    public void loginWithLockedOutUser() {
         WebDriver driver = new ChromeDriver();
+        driver.get(BASE_URL);
 
-        String baseUrl = "https://www.saucedemo.com/";
-        driver.get(baseUrl);
+        login(driver, "locked_out_user", "secret_sauce");
 
+        validateErrorMessage(driver, LOCKED_OUT_ERROR_MESSAGE);
 
-        driver.findElement(By.cssSelector("[data-test=\"username\"]")).sendKeys("locked_out_user");
-        driver.findElement(By.cssSelector("[data-test=\"password\"]")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test=\"login-button\"]")).click();
+        driver.close();
+        driver.quit();
+    }
+    @DataProvider(name = "loginData")
+    public static Object[][] loginData() {
+        return new Object[][] {
+                {"standard_user", "incorrect_password", NO_MATCH_CREDENTIALS_ERROR_MESSAGE},
+                {"incorrect_username", "secret_sauce", NO_MATCH_CREDENTIALS_ERROR_MESSAGE},
+                {"incorrect_username", "incorrect_password", NO_MATCH_CREDENTIALS_ERROR_MESSAGE},
+                {"", "secret_sauce", EMPTY_USERNAME_ERROR_MESSAGE},
+                {"standard_user", "", EMPTY_PASSWORD_ERROR_MESSAGE},
+                {"", "", EMPTY_USERNAME_ERROR_MESSAGE}
+        };
+    }
+    @Test(dataProvider = "loginData", testName = "Validate login scenarios")
+    public void loginTest(String username, String password, String errorMessage) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(BASE_URL);
 
-        String actualErrorMessage = driver.findElement(By.cssSelector("[data-test='error']")).getText();
-        String expectedErrorMessage = "Epic sadface: Sorry, this user has been locked out.";
+        login(driver, username, password);
 
-        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+        validateErrorMessage(driver, errorMessage);
+
         driver.close();
         driver.quit();
     }
 
+    private static void login(WebDriver driver, String username, String password) {
+        driver.findElement(By.cssSelector("[data-test=\"username\"]")).sendKeys(username);
+        driver.findElement(By.cssSelector("[data-test=\"password\"]")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-test=\"login-button\"]")).click();
+    }
+
+    private static void validateErrorMessage(WebDriver driver, String expectedErrorMessage) {
+        String actualErrorMessage = driver.findElement(By.cssSelector("[data-test='error']")).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, "Error message is incorrect");
+    }
 }
--- src/test/java/tests/loginTests/positiveTests/LoginWithStandardUserTest.java
+++ src/test/java/tests/loginTests/positiveTests/LoginWithStandardUserTest.java
@@ -4,72 +4,53 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginWithStandardUserTest {
+    private static final String BASE_URL = "https://www.saucedemo.com/";
+    private static final String PRODUCTS_PAGE_URL = "https://www.saucedemo.com/inventory.html";
 
-
-    @Test
-    public void loginWithStandardUser() {
-        WebDriver driver = new ChromeDriver();
-
-        String baseUrl = "https://www.saucedemo.com/";
-        driver.get(baseUrl);
-
-        String actualUrl = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualUrl, baseUrl);
-
-        driver.findElement(By.cssSelector("[data-test=\"username\"]")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test=\"password\"]")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test=\"login-button\"]")).click();
-
-
-        String productsPageUrl = "https://www.saucedemo.com/inventory.html";
-        actualUrl = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualUrl, productsPageUrl);
-
-
-        String title = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-        String expectedTitle = "Products";
-
-        Assert.assertEquals(title, expectedTitle);
-
-        driver.close();
-        driver.quit();
-
+    @DataProvider(name = "userCredentials")
+    public static Object[][] userCredentials() {
+        return new Object[][]{
+                {"standard_user", "secret_sauce"},
+                {"visual_user", "secret_sauce"},
+                {"problem_user", "secret_sauce"},
+                {"performance_glitch_user", "secret_sauce"},
+                {"error_user", "secret_sauce"},
+        };
     }
 
-    @Test
-    public void loginWithVisualUser() {
+    @Test(dataProvider = "userCredentials")
+    public void loginTest(String username, String password) {
         WebDriver driver = new ChromeDriver();
+        driver.get(BASE_URL);
 
-        String baseUrl = "https://www.saucedemo.com/";
-        driver.get(baseUrl);
+        validateUrl(driver, BASE_URL);
 
-        String actualUrl = driver.getCurrentUrl();
+        login(driver, username, password);
 
-        Assert.assertEquals(actualUrl, baseUrl);
-
-        driver.findElement(By.cssSelector("[data-test=\"username\"]")).sendKeys("visual_user");
-        driver.findElement(By.cssSelector("[data-test=\"password\"]")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test=\"login-button\"]")).click();
-
-
-        String productsPageUrl = "https://www.saucedemo.com/inventory.html";
-        actualUrl = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualUrl, productsPageUrl);
-
-
-        String title = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
-        String expectedTitle = "Products";
-
-        Assert.assertEquals(title, expectedTitle);
+        validateUrl(driver, PRODUCTS_PAGE_URL);
+        validateTitle(driver, "Products");
 
         driver.close();
         driver.quit();
+    }
 
+    private static void login(WebDriver driver, String username, String password) {
+        driver.findElement(By.cssSelector("[data-test=\"username\"]")).sendKeys(username);
+        driver.findElement(By.cssSelector("[data-test=\"password\"]")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-test=\"login-button\"]")).click();
+    }
+
+    private static void validateUrl(WebDriver driver, String expectedUrl) {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "URL is incorrect");
+    }
+
+    private static void validateTitle(WebDriver driver, String expectedTitle) {
+        String title = driver.findElement(By.cssSelector("[class=\"title\"]")).getText();
+        Assert.assertEquals(title, expectedTitle, "Title is incorrect");
     }
 }
